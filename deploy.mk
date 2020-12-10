609c0f8f8ce70f5f8517d33e20b5301769e317ca
duanhongyi <duanhongyi@doopai.com>
2020-12-10 15:50:39 +0800

    chore(influxdb): remove docker keyword from charts

--- deploy.mk
+++ deploy.mk
@@ -1,8 +1,8 @@
 install:
-	helm upgrade influxdb charts/influxdb --install --namespace drycc --set influxdb.org=${IMAGE_PREFIX},influxdb.docker_tag=${VERSION}
+	helm upgrade influxdb charts/influxdb --install --namespace drycc --set influxdb.org=${IMAGE_PREFIX},influxdb.image_tag=${VERSION}
 
 upgrade: 
-	helm upgrade influxdb charts/influxdb --namespace drycc --set influxdb.org=${IMAGE_PREFIX},influxdb.docker_tag=${VERSION}
+	helm upgrade influxdb charts/influxdb --namespace drycc --set influxdb.org=${IMAGE_PREFIX},influxdb.image_tag=${VERSION}
 
 uninstall:
 	helm delete influxdb
